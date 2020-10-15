3d41b5063cded615a989911a163bc3368f7914b3
gaba1 <bruxblank@gmail.com>
2020-10-15 16:37:09 +0200

    slider

--- components/SliderSection.js
+++ components/SliderSection.js
@@ -1,32 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
 import menu from '../json/menu.json'
-
 import SliderTabs from './SliderTabs'
 
-import React from 'react'
 
 
 const SliderSection = () => {
 
-    const menuList = menu
-    // console.log("SliderSection -> menuList", menuList)
-    
-    
-        
-    
 
+    const [activeTab, setActiveTab] = useState(0)
+    const menuList = menu
+    
     return (
         <div className="slider-section">
             {menuList && menuList.map((item, index) =>{
                 const firstTitle = item.name
-                
                 const secondLevel = item.children 
-                // console.log("SliderSection -> secondLevel", secondLevel)
                 
                 return (
                     <div className="slider-item" key={`${item.name}-${index}`}>
                         <h1 className="slider-title">{firstTitle}</h1>
                         {/* Second level Tabs */}
-                        <SliderTabs />
+                        <SliderTabs 
+                            secondLevel={secondLevel}
+                            />
                     </div>
                 )
             })}
--- components/SliderTabs.js
+++ components/SliderTabs.js
@@ -1,41 +1,110 @@
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import {useState} from 'react'
-const SliderTabs = () => {
+import Swiper from 'react-id-swiper';
+// import 'swiper/css/swiper.css';
+import { useState, useEffect, Fragment } from 'react'
 
+const SliderTabs = (props) => {
+
+    // States
     const [value, setValue] = useState(0)
+    const [index, setIndex ] = useState(0)
+    // console.log("SliderTabs -> index", index)
+    // console.log("SliderTabs -> thirdLevel", thirdLevel)
 
+    // Variables
+    const secondLevel = props.secondLevel
+    console.log("SliderTabs -> secondLevel", secondLevel[0])
+    
+    // Settings for Swiper
+    const params = {
+        freeMode: true,
+        spaceBetween: 5,
+        freeModeMomentum: true,
+        freeModeMomentumRatio: .4,
+        freeModeMomentumVelocityRatio: .8,
+        preventClicks: true,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1.5,
+                spaceBetween: 5
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2.5,
+                spaceBetween: 5
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 3.5,
+                spaceBetween: 5
+            },
+            768: {
+                slidesPerView: 4.5,
+                spaceBetween: 10
+            }
+        }
+    }
+
+    // Functions
     const handleChange = (event, newValue) => {
         setValue(newValue);
-      };
+    };
 
-      function a11yProps(index) {
+    function a11yProps(index) {
         return {
             id: `scrollable-auto-tab-${index}`,
             'aria-controls': `scrollable-auto-tabpanel-${index}`,
-            };
-        }
+        };
+    }
+
+    const handleTab = (index) => {
+        setIndex(index)
+    }
+
     return (
-        <AppBar position="static" color="default">
-                            <Tabs
-                                value={value}
-                                onChange={handleChange}
-                                indicatorColor="primary"
-                                textColor="primary"
-                                variant="scrollable"
-                                scrollButtons="auto"
-                                aria-label="scrollable auto tabs example"
-                            >
-                                <Tab label="Item One" {...a11yProps(0)} />
-                                <Tab label="Item Two" {...a11yProps(1)} />
-                                <Tab label="Item Three" {...a11yProps(2)} />
-                                <Tab label="Item Four" {...a11yProps(3)} />
-                                <Tab label="Item Five" {...a11yProps(4)} />
-                                <Tab label="Item Six" {...a11yProps(5)} />
-                                <Tab label="Item Seven" {...a11yProps(6)} />
-                            </Tabs>
-                            </AppBar>
+        <Fragment>
+                <AppBar position="static" color="default">
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="scrollable"
+                        scrollButtons="auto"
+                        aria-label="scrollable auto tabs example"
+                    >
+                        {secondLevel && secondLevel.map((secondItem, secondIndex) => {
+                            const secondTitle = secondItem.name
+                            return (
+                                <Tab 
+                                    label={secondTitle} 
+                                    {...a11yProps(0)} 
+                                    key={`tab-${secondIndex}`} 
+                                    data={secondIndex}
+                                    onClick={()=>handleTab(secondIndex)}/>
+                            )
+                        })}
+                    </Tabs>
+                </AppBar>
+                <Swiper {...params}>
+                    {secondLevel && secondLevel.length > 0 && secondLevel[index].children.map((thirdItem, thirdIndex) => {
+                        let cardItem = thirdItem
+                        return (
+                            <div className="slider-item" key={`card-${thirdIndex}`} >
+                                {cardItem.name}
+                            </div>
+                        )
+
+                    })}
+                    {/* {thirdCategories.length > 0 && thirdCategories.map((category, index) => { */}
+                    {/* return ( */}
+                    {/* )
+                })} */}
+            </Swiper>
+        </Fragment>
     )
 }
 export default SliderTabs
